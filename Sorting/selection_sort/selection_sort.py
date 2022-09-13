e3abd356e29f80cc2c4750a3a7f81250820457f7
Reece Lim <lim.reecesergei@gmail.com>
2022-09-14 00:38:54 +0800

    Move comments for readability

--- Sorting/selection_sort/selection_sort.py
+++ Sorting/selection_sort/selection_sort.py
@@ -3,8 +3,8 @@
 
 # Checks for the smallest value in the array
 def findSmallest(arr):
-    smallest = arr[0]       # Stores the smallest value
-    smallest_index = 0      # Stores the index of the smallest value
+    smallest = arr[0]               # Stores the smallest value
+    smallest_index = 0              # Stores the index of the smallest value
     for i in range(1, len(arr)):
         if arr[i] < smallest:
             smallest = arr[i]
@@ -16,11 +16,11 @@ def findSmallest(arr):
 def selectionSort(arr):
     newArr = []
     for i in range(len(arr)):
-        smallest = findSmallest(arr)      # Finds the smallest value
-        newArr.append(arr.pop(smallest))  # Then adds it to the new array
+        smallest = findSmallest(arr)            # Finds the smallest value
+        newArr.append(arr.pop(smallest))        # Then adds it to the new array
 
     return newArr
     
 # Test Cases
-print(selectionSort([5, 3, 6, 2, 10]))  # [2, 3, 5, 6, 10]
-print(selectionSort([9, 1, 3, 12, 8]))  # [1, 3, 8, 9, 12]
+print(selectionSort([5, 3, 6, 2, 10]))          # [2, 3, 5, 6, 10]
+print(selectionSort([9, 1, 3, 12, 8]))          # [1, 3, 8, 9, 12]
