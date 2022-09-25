986a32669fca47f3037ff804e691bae3e2443b50
Reece Lim <lim.reecesergei@gmail.com>
2022-09-25 23:41:05 +0800

    Finish dijkstra.py

--- graph/dijkstra/dijkstra.py
+++ graph/dijkstra/dijkstra.py
@@ -1,17 +1,55 @@
-# Dijkstra's Algorithm
-# Worst Case: ---
+# Dijktra's Algorithm
+# Worst Case: O(V^2)
 
+def dijkstra():
+    node = find_lowest_cost_node(costs)
+
+    while node is not None:
+        cost = costs[node]
+        neighbors = graph[node]
+
+        for n in neighbors.keys():
+            new_cost = cost + neighbors[n]
+
+            if new_cost < costs[n]:
+                costs[n] = new_cost
+                parents[n] = node
+
+        processed.append(node)
+        node = find_lowest_cost_node(costs)
+
+    print_output()
+
+
+# Find the lowest cost node
 def find_lowest_cost_node(costs):
     lowest_cost = float("inf")
     lowest_cost_node = None
+
     for node in costs:
         cost = costs[node]
+
         if cost < lowest_cost and node not in processed:
             lowest_cost = cost
             lowest_cost_node = node
+
     return lowest_cost_node
 
-# Graphs
+
+# Print the path from start to fin
+def print_output():
+    parent = "fin"
+    path = ["fin"]
+
+    while parent != "start":
+        path.append(parents[parent])
+        parent = parents[parent]
+
+    path.reverse()
+    print(" -> ".join(path))
+
+
+# Graph
 graph = {}
 graph["start"] = {}
 graph["start"]["a"] = 6
@@ -42,20 +80,9 @@ parents["b"] = "start"
 parents["fin"] = None
 
 
-# Array to keep track processed nodes
+# Processed Array (Will be used to store the nodes that have been processed)
 processed = []
 
-node = find_lowest_cost_node(costs)
-while node is not None:
-    cost = costs[node]
-    neighbors = graph[node]
-    for n in neighbors.keys():
-        new_cost = cost + neighbors[n]
-        if new_cost < costs[n]:
-            costs[n] = new_cost
-            parents[n] = node
-    processed.append(node)
-    node = find_lowest_cost_node(costs)
-
-# I am tired, will continue tomorrow lol
-# Been doing this since 10pm - 1:30am
+dijkstra()
+# Output
+# start -> b -> a -> fin
